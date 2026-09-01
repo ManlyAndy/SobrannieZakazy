28d63547c45c32c535b27570e82ed68b3cf31220
ManlyAndy <ike535.andrey@gmail.com>
2026-09-01 08:10:43 +0300

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  PROXY_URL: window.location.origin,
+
+  STATUS_NOT_COLLECTED_NAME: "Не собрано",
+  STATUS_URGENT_NAME: "Срочнее некуда",
+  STATUS_COLLECTED_NAME: "Собрано",
+
+  PLACES_FIELD_NAME: "Количество мест",
+  PICKER_FIELD_NAME: "Имя Сборщика",
+
+  // Заполните реальным списком сборщиков.
+  PICKER_NAMES: [
+    // "Иван Иванов",
+    // "Пётр Петров"
+  ]
+};
